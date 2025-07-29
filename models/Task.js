9c692d75f4bb6d9d34234ce1d03831b3e5b51c58
mrunalptiwase <mrunaltiwase19@gmail.com>
2025-07-29 07:01:27 +0530

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
 
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  reward: {
+    type: Number,
+    required: true
+  },
+  link: String,
+  type: {
+    type: String,
+    enum: ["visit", "download", "signup"],
+    default: "visit"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
